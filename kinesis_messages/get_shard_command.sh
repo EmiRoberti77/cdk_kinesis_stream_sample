813d9948c8fd1e2795fbd2d18b88acc1b526fdbb
EmiRoberti77 <emi@datamotion.ai>
2025-01-24 08:38:20 +0000

    get shard command

---
+++ kinesis_messages/get_shard_command.sh
@@ -0,0 +1,3 @@
+aws kinesis get-shard-iterator --stream-name emi-kinesis-stream --shard-id shardId-000000000001 --shard-iterator-type TRIM_HORIZON
+
+
--- kinesis_messages/put_command.sh
+++ kinesis_messages/put_command.sh
@@ -1,1 +1,1 @@
-aws kinesis put-record --stream-name emi-kinesis-stream --data "aGVsbG8=" --partition-key PartitionKey2
+aws kinesis put-record --stream-name emi-kinesis-stream --data "aGVsbG8=" --partition-key PartitionKey1
